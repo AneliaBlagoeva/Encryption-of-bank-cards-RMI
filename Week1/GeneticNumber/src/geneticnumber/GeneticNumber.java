8cff75052561a3b98f619c29612cba866cb98d13
Anelia Blagoeva <arblagoeva@gmail.com>
2017-10-08 10:28:18 +0300

    week 1

---
+++ Week1/GeneticNumber/src/geneticnumber/GeneticNumber.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package geneticnumber;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author user
+ */
+public class GeneticNumber {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //declaration
+        Scanner kbd;
+        
+        int userInput;
+        //initialization  
+        
+        //processing
+        
+        //output
+
+    }
+    
+}
