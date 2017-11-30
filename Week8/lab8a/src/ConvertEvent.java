222020ff329a3cadca7c76ff7af53ae4e5c9ec0b
Anelia <arblagoeva@gmail.com>
2017-11-30 16:20:28 +0200

    problem4 - currency convert

---
+++ Week8/lab8a/src/ConvertEvent.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package currencyconverter;
+
+/**
+ *
+ * @author Haemimont
+ */
+public class ConvertEvent {
+
+    private double value;
+
+    /**
+     * Get the value of value
+     *
+     * @return the value of value
+     */
+    public double getValue() {
+        return value;
+    }
+
+    /**
+     * Set the value of value
+     *
+     * @param value new value of value
+     */
+    public void setValue(double value) {
+        if (value < 0) {
+            this.value = 0;
+        } else {
+            this.value = value;
+        }
+    }
+
+    public ConvertEvent(double value) {
+        setValue(value);
+    }
+    
+    
+
+}
---
+++ Week8/lab8a/src/Converter.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package currencyconverter;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Converter {
+    
+    private ConvertListener cl;
+    
+    private double value;
+
+    /**
+     * Get the value of value
+     *
+     * @return the value of value
+     */
+    public double getValue() {
+        return value;
+    }
+
+    /**
+     * Set the value of value
+     *
+     * @param value new value of value
+     */
+    public void setValue(double value) {
+        if(value<0){
+            this.value=0;
+        }else{
+        this.value = value;
+        }
+    }
+
+    public Converter(double value) {
+        setValue(value);
+    }
+
+    
+    public void addListener(ConvertListener listener){
+        this.cl=listener;
+    }
+    
+    public void convertCurrency(ConvertEvent ev){
+        cl.convertPerformed(ev);
+    }
+
+    
+    //TO DO: implement inner class that implements the interface
+}
