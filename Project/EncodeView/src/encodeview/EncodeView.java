fbaf27f8e774f9dbac5891db0d3943a7260a37f8
unknown <arblagoeva@gmail.com>
2018-01-30 00:26:50 +0200

    Changes to EncodeView

--- Project/EncodeView/src/encodeview/EncodeView.java
+++ Project/EncodeView/src/encodeview/EncodeView.java
@@ -3,9 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package encodeview;
 
 import javafx.application.Application;
+import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -27,6 +29,7 @@ public class EncodeView extends Application {
         stage.show();
     }
 
+    
     /**
      * @param args the command line arguments
      */
---
+++ Project/EncodeView/src/encodeview/FXMLDocumentController.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package encodeview;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author Anelia
+ */
+public class FXMLDocumentController extends AnchorPane implements Initializable {
+
+    @FXML
+    public AnchorPane pane;
+    @FXML
+    public Button btnEncode;
+    @FXML
+    public Label lblResult;
+    @FXML
+    public Button btnDecode;
+    @FXML
+    public TextField txtCode;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
