89c60c040b7d7ead69cae742d64961b76533c0c8
unknown <arblagoeva@gmail.com>
2018-02-07 23:41:25 +0200

    Changes to back end logic in windows for handling has rights filter

--- Project/LoginApp/src/loginapp/FXMLDocumentController.java
+++ Project/LoginApp/src/loginapp/FXMLDocumentController.java
@@ -11,10 +11,16 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import server.Login;
 
 /**
@@ -33,27 +39,38 @@ public class FXMLDocumentController implements Initializable {
     private TextField txtUsername;
     @FXML
     private Button btnLogin;
-    
-    public Login login=null;
+
+    public Login login = null;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-
+    }
 
     @FXML
     private void onBtnLoginClicked(ActionEvent event) {
-        try{
-        Users u=DeserealizationWarpper.deserealization();
-        String userName=txtUsername.getText();
-        String password=txtPassword.getText();
-        login=new Login(userName,password);
-        login.checkCredentials(u);
-        }catch(Exception e){
+        try {
+            Users u = DeserealizationWarpper.deserealization();
+            String userName = txtUsername.getText();
+            String password = txtPassword.getText();
+            login = new Login(userName, password);
+            if (!(login.checkCredentials(u))) {
+                try {
+                    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ErrorWindow.fxml"));
+                    Parent root1 = (Parent) fxmlLoader.load();
+                    Stage stage = new Stage();
+                    stage.setScene(new Scene(root1));
+                    stage.show();
+                } catch (Exception e) {
+                    System.out.println("Error.");
+                }
+            }else{
+                        System.out.println("TODO: Load encode view");
+                        }
+        } catch (Exception e) {
             System.out.println("Error.");
         }
-        
-        
+
     }
-    
+
 }
